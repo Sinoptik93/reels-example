b1c0289e705961732620f18a4db314f103c06ccc
Sergey Chepukhin <chepukhin.sergey@gmail.com>
2023-03-13 11:34:39 +0500

    master: add webm video

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import video from "./assets/short-1.mp4"
+import videomp4 from "./assets/short-1.mp4"
+import videowebm from "./assets/short-1.webm"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-        <video src={video} width="600" height="300"  autoPlay controls/>
+        <video width="600" height="300" autoPlay controls>
+            <source src={videowebm} type="video/webm"/>
+            {/*<source src={videomp4} type="video/mp4"/>*/}
+        </video>
     </div>
   )
 }
