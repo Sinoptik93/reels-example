01e5512645769af626f99f9b95243a44fcc86e2a
Sergey Chepukhin <chepukhin.sergey@gmail.com>
2023-03-20 15:19:43 +0500

    master: fix main

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import {useState} from 'react'
+import { useState } from 'react'
+import { Link } from "react-router-dom";
 import 'swiper/swiper-bundle.min.css';
 import './App.scss'
-import videomp4 from "./assets/short-1.mp4"
-import videowebm from "./assets/short-1.webm"
-import videomp42 from "./assets/short-2.mp4"
-import videowebm2 from "./assets/short-2.webm"
-import videomp43 from "./assets/short-3.mp4"
-import videowebm3 from "./assets/short-3.webm"
-import overlay from "./assets/overlay.png"
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
 
 
 
@@ -17,7 +9,10 @@ function App() {
     const [count, setCount] = useState(0)
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     return (
-        <div>Main</div>
+        <>
+            <h1>Main</h1>
+            <Link to="/reels">Reels</Link>
+        </>
     )
 }
 
