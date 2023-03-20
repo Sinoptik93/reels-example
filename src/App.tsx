c4f5041a74e58853d03a94d7d2e12ac9b87c71fa
Sergey Chepukhin <chepukhin.sergey@gmail.com>
2023-03-20 12:59:05 +0500

    master: add router

--- src/App.tsx
+++ src/App.tsx
@@ -17,76 +17,7 @@ function App() {
     const [count, setCount] = useState(0)
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     return (
-        <Swiper
-            className="container h-[520px] overflow-hidden max-w-[400px] w-full swiper-backface-hidden"
-            spaceBetween={10}
-            slidesPerView={"auto"}
-            centeredSlides
-            modules={[Pagination]}
-            pagination={{ clickable: true }}
-        >
-            <SwiperSlide className="rounded-2xl overflow-hidden w-[320px] h-[480px] relative">
-                {
-                    isMobile
-                        ? null
-                        : (
-                            <>
-                                <div className="orange-circle"/>
-                                <div className="violet-circle"/>
-                            </>
-                        )
-                }
-
-                <video className="top-0 left-0 min-h-full min-w-full" loop autoPlay muted playsInline >
-                    <source src={videowebm} type="video/webm"/>
-                    <source src={videomp4} type="video/mp4"/>
-                </video>
-                t
-            </SwiperSlide>
-
-            <SwiperSlide className="rounded-2xl overflow-hidden w-[320px] h-[480px]">
-                {
-                    isMobile
-                        ? null
-                        : (
-                            <>
-                                <div className="orange-circle"/>
-                                <div className="violet-circle"/>
-                            </>
-                        )
-                }
-
-                <video className="top-0 left-0 min-h-full min-w-full" loop autoPlay muted playsInline>
-                    <source src={videowebm2} type="video/webm"/>
-                    <source src={videomp42} type="video/mp4"/>
-                </video>
-
-            </SwiperSlide>
-
-
-            <SwiperSlide className="rounded-2xl overflow-hidden w-[320px] h-[480px]">
-                {
-                    isMobile
-                        ? null
-                        : (
-                            <>
-                                <div className="orange-circle"/>
-                                <div className="violet-circle"/>
-                            </>
-                        )
-                }
-
-                <video className="top-0 left-0 min-h-full min-w-full" loop autoPlay muted playsInline>
-                    <source src={videowebm3} type="video/webm"/>
-                    <source src={videomp43} type="video/mp4"/>
-                </video>
-                t
-            </SwiperSlide>
-
-
-
-
-        </Swiper>
+        <div>Main</div>
     )
 }
 
