94ea804e89e5264a7f5af29bcc3aafb3a748e4c2
Sergey Chepukhin <chepukhin.sergey@gmail.com>
2023-04-17 13:36:26 +0500

    astro: update

--- src/components/ClientPath/index.tsx
+++ src/components/ClientPath/index.tsx
@@ -124,13 +124,13 @@ function ClientPath() {
                 </SwiperSlide>
             </Swiper>
 
-            <div className="container relative mobile:hidden tablet:hidden w-full overflow-hidden rounded-2xl">
+            <div className="relative mobile:hidden tablet:hidden w-full overflow-hidden">
                 <div
                     className="absolute z-10 w-[300rem] -left-[135rem] -top-[66rem] rotate-90">
                     <img src={ellipseViolet.src} alt="violet"/>
                 </div>
 
-                <video className="top-24 left-0 desktop:h-[54.8125rem] tablet:h-[26.875rem] mx-auto absolute mobile:hidden object-video-frame" loop autoPlay muted playsInline>
+                <video className="absolute-x-center w-[100vw] max-w-[130rem] desktop:h-[54.8125rem] tablet:h-[26.875rem] mx-auto absolute mobile:hidden object-video-frame" loop autoPlay muted playsInline>
                     <source src={pathFull} type="video/webm"/>
                     <source src={pathFullExtra} type="video/mp4"/>
                 </video>
