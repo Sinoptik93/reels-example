00074ccc71710507f281404de5112b86d03d2bd1
Sergey Chepukhin <chepukhin.sergey@gmail.com>
2023-05-04 19:14:36 +0500

    astro: update

--- src/scripts/fullPage.ts
+++ src/scripts/fullPage.ts
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     if (window.innerWidth < 500) {
         const fullpageInstance = new FullPage('#fullpage', {
-            licenseKey: "gplv3-license",
+            licenseKey: "59L98-1CBMK-YHEG7-AO0LK-NTLVM",
             scrollHorizontallyKey: '6A7AE941-5C404F92-B32196A3-B8F57BF4',
             scrollHorizontally: true,
 
